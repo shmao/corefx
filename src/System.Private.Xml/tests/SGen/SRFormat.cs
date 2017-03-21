f73519d37e2bda9db3d77cf0c0edeafd9498b304
Shin Mao <shmao@microsoft.com>
2017-03-21 13:23:13 -0700

    SGen closure.

---
+++ src/System.Private.Xml/tests/SGen/SRFormat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class SRFormat
+{
+    public static string Format(params object[] objects)
+    {
+        return objects[0].ToString();
+    }
+}
+
