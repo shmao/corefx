8c4a31e86933651c505e11e9408426afefc34110
Xing Mao <shmao@microsoft.com>
2017-10-17 11:28:01 -0700

    Added ReadFrom and WriteTo

--- src/System.ServiceModel.Syndication/src/System/ServiceModel/Syndication/SyndicationFeedFormatter.cs
+++ src/System.ServiceModel.Syndication/src/System/ServiceModel/Syndication/SyndicationFeedFormatter.cs
@@ -44,12 +44,16 @@ namespace System.ServiceModel.Syndication
 
         public abstract bool CanRead(XmlReader reader);
 
+        public abstract void ReadFrom(XmlReader reader);
+
         public abstract Task ReadFromAsync(XmlReader reader, CancellationToken ct);
 
         public override string ToString()
         {
             return string.Format(CultureInfo.CurrentCulture, "{0}, SyndicationVersion={1}", this.GetType(), this.Version);
         }
+
+        public abstract void WriteTo(XmlWriter writer);
 
         public abstract Task WriteToAsync(XmlWriter writer, CancellationToken ct);
 
